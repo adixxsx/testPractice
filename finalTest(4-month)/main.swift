064dcdff133d946d9ddd92969491bfb06b171cb8
adixxsx <ad1letsa5irkulov@gmail.com>
2024-02-05 20:26:41 -0800

    1

--- finalTest(4-month)/main.swift
+++ finalTest(4-month)/main.swift
@@ -7,5 +7,31 @@
 
 import Foundation
 
-print("Hello, World!")
+//print("Hello, World!")
 
+protocol Greeter {
+    func greet() -> String
+}
+
+struct Person: Greeter {
+    var name1: String
+    
+    func greet() -> String {
+        return "Привет, я \(name1)!"
+    }
+}
+
+struct Human: Greeter {
+    var name2: String
+    
+    func greet() -> String {
+        return "Салам! Я \(name2)!"
+    }
+}
+
+
+let person = Person(name1: "Маткасым")
+let human = Human(name2: "Мурад")
+
+print(person.greet())
+print(human.greet())
